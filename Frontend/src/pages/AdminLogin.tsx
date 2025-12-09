c0a6952baa7c1e254461a730ef45d37b4bfe5285
rupesh0001-tech <rupeshcoding01@gmail.com>
2025-12-09 23:14:36 +0530

    fixing some ninor issues

--- Frontend/src/pages/AdminLogin.tsx
+++ Frontend/src/pages/AdminLogin.tsx
@@ -1,12 +1,6 @@
 import AdminLoginBox from "../components/AdminLoginBox";
 
-
-
 const AdminLogin = () => {
-
-
-
-
 
   return (
     <div className="flex flex-col h-screen items-center justify-center w-full ">
