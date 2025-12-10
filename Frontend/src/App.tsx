8e3243afa25c2eb138e0b556028226c9453db9f2
rupesh0001-tech <rupeshcoding01@gmail.com>
2025-12-10 18:58:02 +0530

    feat: add route for AdminDashboard in the application

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import AdminLogin from "./pages/AdminLogin"
+import AdminDashboard from "./pages/AdminDashboard"
 
 
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
     </Routes>
   )
 }
