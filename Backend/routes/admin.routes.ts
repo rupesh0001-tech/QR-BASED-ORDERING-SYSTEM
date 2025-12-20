41396e43ac092c1e326e3a80487f543836106424
rupesh0001-tech <rupeshcoding01@gmail.com>
2025-12-20 21:15:26 +0530

    feat: add create-table route to admin routes

--- Backend/routes/admin.routes.ts
+++ Backend/routes/admin.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { adminLogin } from '../controllers/adminLogin';
+import { createTable } from '../controllers/genrate.table.qr';
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ const router = express.Router();
 
 // POST api/admin/login
 router.post('/login',  adminLogin);
+router.post('/create-table',  createTable);
 
 export default router;
