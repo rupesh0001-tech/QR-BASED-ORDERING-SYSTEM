987b8e9786671dcdd375c313aac5ebaf16a9cc56
rupesh0001-tech <rupeshcoding01@gmail.com>
2025-12-21 19:02:18 +0530

    feat: Refactor MainCards component layout and improve image handling

--- Frontend/src/components/User Menu/MainCards.tsx
+++ Frontend/src/components/User Menu/MainCards.tsx
@@ -27,12 +27,32 @@ const MainCards = ({
   status,
 }: MenuItemI) => {
   return (
-    <div>
-      <div className=" flex gap-2">
-        <div className=" flex justify-center items-center w-30 h-30 bg-[#fefbe5] p-2">
-          <img decoding="async" loading="lazy" className=" h-20 w-20" src={image} alt="" />
+    <div className="flex px-3 gap-6 min-h-[125px] text-start items-center justify-between shadow-md rounded-2xl ">
+      {/* Image */}
+      <div className="w-30 h-27 flex justify-center items-center bg-[#fefbe5] rounded-2xl">
+        <img src={image} alt={name} className="w-30 object-contain" />
+      </div>
+
+      {/* Content */}
+      <div className="py-2 px w-[70%] flex flex-col ">
+        <div>
+          <h1 className="text-md font-bold text-neutral-800">{name}</h1>
+
+          <p className="text-sm font-medium text-neutral-700 line-clamp-2">
+            {description}
+          </p>
         </div>
-        <h1 className=" text-md font-bold text-center ">{name}</h1>
+
+        {/* Bottom row */}
+        <div className="flex w-full items-center  gap-4 mt-4 ">
+          <span className="bg-orange-300 px-3 py-2 rounded-xl text-sm font-semibold">
+            ₹{price}
+          </span>
+
+          <button className="bg-blue-950 text-white px-3.5 py-3 rounded-xl text-xs font-semibold">
+            Add
+          </button>
+        </div>
       </div>
     </div>
   );
