3bae8fa2c747d00f7da7eba2773de2d4402c2f05
rupesh0001-tech <rupeshcoding01@gmail.com>
2025-12-24 16:55:44 +0530

    chnages

--- Frontend/src/components/User Menu/Cart.tsx
+++ Frontend/src/components/User Menu/Cart.tsx
@@ -1,15 +1,23 @@
 import { ShoppingCart } from "lucide-react";
+
 import { useSelector } from "react-redux";
 
 
 
-const Cart = () => {
-    const countOfItems = useSelector((state : any) => state.menuReducers.Cart )
+
+const Cart = ({handleCartClick } : any ) => {
+    const cartItems = useSelector((state : any) => state.menuReducers.Cart );
+    
+
+   
+    
+    let cartItemsCount = cartItems.reduce((total : any, item : any  ) => total + item.quantity , 0)
+    
   return (
     <div className="fixed bottom-6 right-6 bg-white rounded-2xl w-15 h-15 flex justify-center items-center shadow-md ">
-      <div>
+      <div onClick={ () => handleCartClick()} >
         <ShoppingCart stroke={"black"} strokeWidth={2} />
-        <span className=" bg-orange-400 h-6 w-6 px-1 rounded-full text-sm font-semibold flex justify-center items-center absolute top-1 right-0"> {countOfItems.length} </span>
+        <span className=" bg-orange-400 h-6 w-6 px-1 rounded-full text-sm font-semibold flex justify-center items-center absolute top-1 right-0"> {cartItemsCount} </span>
       </div>
     </div>
   );
